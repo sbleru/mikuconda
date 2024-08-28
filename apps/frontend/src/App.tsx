c1052c153faa8d415c5fbebaa33fd79b37d16cd3
sbleru <usareru@gmail.com>
2024-08-28 22:08:02 +0900

    feat: Use react-three-fiber

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Canvas } from "@react-three/fiber";
 
 const App: React.FC = () => {
   return (
@@ -6,6 +7,14 @@ const App: React.FC = () => {
       <header id="header">
         <h1>Mikuconda</h1>
       </header>
+      <Canvas>
+        <mesh>
+          <boxGeometry args={[2, 2, 2]} />
+          <meshPhongMaterial />
+        </mesh>
+        <ambientLight intensity={0.1} />
+        <directionalLight position={[0, 0, 5]} color="#39c5bb" />
+      </Canvas>
     </div>
   );
 };
