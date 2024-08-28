51125450ab5e162a19f908875649bab1140c37af
sbleru <usareru@gmail.com>
2024-08-28 21:53:53 +0900

    feat: Use react

---
+++ apps/frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <header id="header">
+        <h1>Mikuconda</h1>
+      </header>
+    </div>
+  );
+};
+
+export default App;
